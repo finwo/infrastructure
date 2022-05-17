3af208aad473ae8651cf44ff91feb8045f8a0bde
Robin Bron <robin@finwo.nl>
2022-05-17 18:48:29 +0200

    Removed broken allow_other in worker nfs mount

--- config/module/nfs-client/install.sh
+++ config/module/nfs-client/install.sh
@@ -4,4 +4,4 @@ apt-get update -yyq
 apt-get install -yqq nfs-common
 rm -rf /mnt/pool
 mkdir -p /mnt/pool
-mount -t nfs -o allow_other ${NFS_AGENT}:/srv/storage /mnt/pool
+mount -t nfs ${NFS_AGENT}:/srv/storage /mnt/pool
